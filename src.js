cc1d8c13bc0bbcfcd98a8656e1342fe5fe629430
Yon Roa <91107387+yonroa@users.noreply.github.com>
2021-11-25 21:01:22 -0500

    Add files via upload

--- src.js
+++ src.js
@@ -1,136 +1,99 @@
-// const fetch = require('fetch-simulator');
-// const fetch = require('cross-fetch');
-const limits = [
-    { "etiqueta": "Monoxido de carbono", "de": 0, "hasta": 10 },
-    { "etiqueta": "Dioxido de carbono", "de": 0, "hasta": 20 },
-    { "etiqueta": "Hidrocarburos", "de": 0, "hasta": 10000 },
-    { "etiqueta": "Oxigeno", "de": 0, "hasta": 22 }    
-]
-
-const ranges_parameters = {
-    "Parametro CO en rango estandar": 10,
-    "Parametro CO2 en rango estandar":20,
-    "Parametro HC en rango estandar": 10000,
-    "Parametro O2 en rango estandar": 22
-}
+const fetch = require('fetch-simulator');
 
 const calcularPocentajes = async(
     valorCO,
     valorCO2,
     valorHC,
-    valor02
+    valorO2
 ) => {
-    // make a fetch request to the api
-    let response = await fetch(" https://misiontic2022upb.vercel.app/api/emission-measurement/limits");
+    let response = await fetch("https://misiontic2022upb.vercel.app/api/emission-measurement/ranges-parameters");
     let limits = await response.json();
+    console.log(limits)
+    console.log(Object.values(limits))
+    const limites = Object.values(limits)
+    const [ limiteCO, limiteCO2, limiteHC, limiteO2 ] = limites;
+
+    let porcentajeCO = limiteCO == 0 ? (valorCO * 100) / limiteCO : 0;
+    let porcentajeCO2 = limiteCO2 == 0  ? (valorCO2 * 100) / limiteCO2 : 0;
+    let porcentajeHC = limiteHC == 0 ? (valorHC * 100) / limiteHC : 0;
+    let porcentajeO2 = limiteO2 == 0 ? (valorO2 * 100) / limiteO2 : 0;
+    
     return {
-        porcentajeCO,
-        porcentajeCO2,
-        porcentajeHC,
-        porcentaje02,
+        "porcentajeCO":porcentajeCO,
+        "porcentajeCO2":porcentajeCO2,
+        "porcentajeHC":porcentajeHC,
+        "porcentajeO2":porcentajeO2,
     };
 };
 
-global.rangosCO = [
-    { etiqueta: 'Parametro CO en rango estandar', de: 0, hasta: 10 },
-    { etiqueta: 'Parametro CO fuera de rango', de: 11, hasta: 15 },
-]
-
-global.rangosCO2 = [
-    { etiqueta: 'Parametro CO2 en rango estandar', de: 0, hasta: 20 },
-    { etiqueta: 'Parametro CO2 fuera de rango', de: 21, hasta: 30 },
-]
-
-global.rangosHC = [
-    { etiqueta: 'Parametro HC en rango estandar', de: 0, hasta: 10000 },
-    { etiqueta: 'Parametro HC fuera de rango', de: 10001, hasta: 11000 },
-]
-
-global.rangosO2 = [
-    { etiqueta: 'Parametro O2 en rango estandar', de: 0, hasta: 22 },
-    { etiqueta: 'Parametro O2 fuera de rango', de: 23, hasta: 30 },
-]
-
 const registrarCO = async(valor) => {
-    let response = await fetch("https://misiontic2022upb.vercel.app/api/emission-measurement/ranges-parameters");
+    let response = await fetch("https://misiontic2022upb.vercel.app/api/emission-measurement/limits");
     let rangoCO = await response.json();
     let candidate = rangoCO[0];
-    if(valor > 15 || valor < 0){
-        console.log("fuera_de_rango")
-        return "fuera_de_rango"
+    console.log(candidate)
+    if (valor >= candidate.de && valor <= candidate.hasta) {
+        console.log('Parametro CO en rango estandar')
+        return ('Parametro CO en rango estandar')
     }
-    else if(valor <= 10 && valor >= 0){
-        const etiquetas = rangosCO.map(item => item.etiqueta)
-        const res = etiquetas.find(el => el == 'Parametro CO en rango estandar')
-        console.log(res)
-        return res
-    }    
     else if(valor >= 11 && valor <= 15){
-        const etiquetas = rangosCO.map(item => item.etiqueta)
-        const res = etiquetas.find(el => el == 'Parametro CO fuera de rango')
-        console.log(res)
-        return res
+        console.log('Parametro CO fuera de rango estandar')
+        return ('Parametro CO fuera de rango estandar') 
     }
+    else{
+        return("fuera_de_rango")
+    } 
 };
 
-// registrarCO(-1);
-
 const registrarCO2 = async(valor) => {
-    let response = await fetch("https://misiontic2022upb.vercel.app/api/emission-measurement/ranges-parameters");
+    let response = await fetch("https://misiontic2022upb.vercel.app/api/emission-measurement/limits");
     let rangoCO2 = await response.json();
     let candidate = rangoCO2[1];
-    if(valor > 30 || valor < 0){
-        return "fuera_de_rango"
+    if (valor >= candidate.de && valor <= candidate.hasta) {
+        console.log('Parametro CO2 en rango estandar')
+        return ('Parametro CO2 en rango estandar')
     }
-    else if(valor <= 20 && valor >= 0){
-        const etiquetas = rangosCO2.map(item => item.etiqueta)
-        const res = etiquetas.find(el => el == 'Parametro CO2 en rango estandar')
-        return res
-    }    
     else if(valor >= 21 && valor <= 30){
-        const etiquetas = rangosCO2.map(item => item.etiqueta)
-        const res = etiquetas.find(el => el == 'Parametro CO2 fuera de rango')
-        return res
+        console.log('Parametro CO2 fuera de rango estandar')
+        return ('Parametro CO2 fuera de rango estandar') 
     }
+    else{
+        return("fuera_de_rango")
+    } 
 };
 
 const registrarHC = async(valor) => {
-    let response = await fetch("https://misiontic2022upb.vercel.app/api/emission-measurement/ranges-parameters");
+    let response = await fetch("https://misiontic2022upb.vercel.app/api/emission-measurement/limits");
     let rangoHC = await response.json();
     let candidate = rangoHC[2];
-    if(valor > 11000 || valor < 0){
-        return "fuera_de_rango"
+    if (valor >= candidate.de && valor <= candidate.hasta) {
+        console.log('Parametro HC en rango estandar')
+        return ('Parametro HC en rango estandar')
     }
-    else if(valor <= 10000 && valor >= 0){
-        const etiquetas = rangosHC.map(item => item.etiqueta)
-        const res = etiquetas.find(el => el == 'Parametro HC en rango estandar')
-        return res
+    else if(valor > 10 && valor <= 15){
+        console.log('Parametro HC fuera de rango estandar')
+        return ('Parametro HC fuera de rango estandar') 
     }
-    else if(valor >=10001){
-        const etiquetas = rangosHC.map(item => item.etiqueta)
-        const res = etiquetas.find(el => el == 'Parametro HC fuera de rango')
-        return res
-    }
+    else{
+        return("fuera_de_rango")
+    } 
 };
 
 const registrarO2 = async(valor) => {
-    let response = await fetch("https://misiontic2022upb.vercel.app/api/emission-measurement/ranges-parameters");
+    let response = await fetch("https://misiontic2022upb.vercel.app/api/emission-measurement/limits");
     let rangoO2 = await response.json();
     let candidate = rangoO2[3];
-    if(valor > 30 || valor < 0){
-        return "fuera_de_rango"
+    if (valor >= candidate.de && valor <= candidate.hasta) {
+        console.log('Parametro O2 en rango estandar')
+        return ('Parametro O2 en rango estandar')
     }
-    if(valor <= 22 && valor >= 0){
-        const etiquetas = rangosO2.map(item => item.etiqueta)
-        const res = etiquetas.find(el => el == 'Parametro O2 en rango estandar')
-        return res
+    else if(valor > 10 && valor <= 15){
+        console.log('Parametro O2 fuera de rango estandar')
+        return ('Parametro O2 fuera de rango estandar') 
     }
-    if(valor >= 23){
-        const etiquetas = rangosO2.map(item => item.etiqueta)
-        const res = etiquetas.find(el => el == 'Parametro O2 fuera de rango')
-        return res
-    }
-}
+    else{
+        return("fuera_de_rango")
+    } 
+};
 
 module.exports.registrarCO = registrarCO;
 module.exports.registrarCO2 = registrarCO2;
